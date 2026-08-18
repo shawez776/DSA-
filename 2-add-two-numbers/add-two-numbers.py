e245e53d1fe5479469cd589d926827ae7fcd7268
Shawez Noor <144467870+shawez776@users.noreply.github.com>
2026-08-18 17:58:39 +0530

    Time: 9 ms (8.88%) | Memory: 19.4 MB (41.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    #submitted by noor
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummyHead = ListNode(0)
         tail = dummyHead
