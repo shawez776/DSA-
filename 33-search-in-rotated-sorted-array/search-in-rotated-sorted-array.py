14687dbcc7843a9aa5fe7d8218f16bb663f3220e
Shawez Noor <144467870+shawez776@users.noreply.github.com>
2026-09-02 18:04:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.02%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        low, high = 0 , n-1
+        for i in range(0,n):
+            if nums[i] == target:
+                return i 
+        return -1
+        
